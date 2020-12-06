e047a69a728b6bdd96597ceaa692964aefdd661c
faysal-ahmed-tarek <freelancer.faysalahmed@gmail.com>
2020-12-06 20:09:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-function App() {
+export default function List() {
+
+  const [Value, change_func] = useState();
+  const [current_item, item_func] = useState([]);
+  function get_item(event) {
+      change_func(event.target.value);
+  }
+  function add_item() {
+    if (Value == undefined || Value== "") {
+      alert("item add kr shala")
+    } else {
+      item_func((val) => {
+          return [...val, todo]
+      })
+      console.log(current_item);
+      change_func("");
+    }
+  }
+  const todo = {
+    Value, id: Math.random()
+  }
+  function delete_item(i) {
+    var result = current_item.filter(e => e.id != i);
+    item_func(result);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+    <section className="container">
+        <div className="list_app">
+          <h1>to do list</h1>
+        <div className="app_body">
+            <input onChange={get_item} type="text" value={Value} />
+            <button onClick={add_item}>add</button>
+          </div>
 
-export default App;
+          <div className="all_list">
+            <ol>
+            {
+              current_item.map((items) => {
+                return <li>{items.Value} <span><i onClick={() => delete_item(items.id)} class="fa fa-trash" aria-hidden="true"></i></span></li>
+              })
+            }
+            </ol>
+          </div>
+        </div>
+          
+    </section>
+      
+    </>
+  )
+}
